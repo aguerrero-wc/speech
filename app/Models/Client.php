3bc32ac0f0e20aa4ee22cc9aaff0110f39421f2a
aguerrero <aguerrero@windowschannel.com>
2025-11-26 17:29:07 -0500

    fix error

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class Client extends Authenticatable
 {
-    protected $fillable = ['cedula', 'name'];
+    use HasUuids;
+    protected $fillable = ['cc'];
     // Esto es importante para que Laravel sepa que no buscamos password
     public function getAuthPassword() {
         return null; 
